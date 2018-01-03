17ad0bae06f183329791ec603d37b4e785d6d5d0
ramonjd <ramonjd@gmail.com>
2018-01-03 18:04:12 +1100

    Reduxifying LocaleSuggestions and adding http call action to the data layer Added i18n state namespace to conform with upcoming localized language names in #20694

---
+++ client/state/data-layer/wpcom/i18n/index.js
@@ -0,0 +1,8 @@
+/** @format */
+/**
+ * Internal dependencies
+ */
+import { mergeHandlers } from 'state/action-watchers/utils';
+import localeSuggestions from './locale-suggestions';
+
+export default mergeHandlers( localeSuggestions );
