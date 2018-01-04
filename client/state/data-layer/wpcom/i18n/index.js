8c2cf35489816315c166f3de0b95d133ca50735c
ramonjd <ramonjd@gmail.com>
2018-01-04 13:20:00 +1100

    Changed file structure under wpcom to reflect the api path Renamed action functions to describe their effects Removed locale-guess test for now, at least until it grows in complexity

--- client/state/data-layer/wpcom/i18n/index.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-/**
- * Internal dependencies
- */
-import { mergeHandlers } from 'state/action-watchers/utils';
-import localeSuggestions from './locale-suggestions';
-
-export default mergeHandlers( localeSuggestions );
